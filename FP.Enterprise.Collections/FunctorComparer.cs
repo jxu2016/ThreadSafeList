c2818ece7727b43b7bafef52f13ea51f4358c683
unknown <jxu@jxu-pc.myriad-development.com>
2015-04-13 15:29:20 -0500

    check in source

---
+++ FP.Enterprise.Collections/FunctorComparer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FP.Enterprise.Collections
+{
+    public sealed class FunctorComparer<T> : IComparer<T>
+    {
+        Comparison<T> comparison;
+
+        public FunctorComparer(Comparison<T> comparison)
+        {
+            this.comparison = comparison;
+        }
+
+        public int Compare(T x, T y)
+        {
+            return comparison(x, y);
+        }
+    }
+
+}
---
+++ Test/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using FP.Enterprise.Collections;
+
+namespace Test
+{
+    public class Example
+    {
+        public static void Main()
+        {
+            // Create a list of parts.
+
+           var parts = new FP.Enterprise.Collections.CustomList<Part>();
+
+            // Add parts to the list.
+            parts.Add(new Part() {PartName = "crank arm", PartId = 1234});
+            parts.Add(new Part() {PartName = "chain ring", PartId = 1334});
+            parts.Add(new Part() {PartName = "regular seat", PartId = 1434});
+            parts.Add(new Part() {PartName = "banana seat", PartId = 1444});
+            parts.Add(new Part() {PartName = "cassette", PartId = 1534});
+            parts.Add(new Part() {PartName = "shift lever", PartId = 1634});
+
+            var threadSafeList = new TheadSafeList<Part>(parts);
+
+            // Write out the parts in the list. This will call the overridden ToString method 
+            // in the Part class.
+            Console.WriteLine();
+            foreach (var aPart in threadSafeList)
+            {
+                Console.WriteLine(aPart);
+            }
+
+            // Check the list for part #1734. This calls the IEquitable.Equals method 
+            // of the Part class, which checks the PartId for equality.
+            Console.WriteLine("\nContains(\"1734\"): {0}",
+                threadSafeList.Contains(new Part { PartId = 1734, PartName = "" }));
+
+            // Insert a new item at position 2.
+            Console.WriteLine("\nInsert(2, \"1834\")");
+            threadSafeList.Insert(2, new Part() { PartName = "brake lever", PartId = 1834 });
+
+
+            //Console.WriteLine(); 
+            foreach (var aPart in threadSafeList)
+            {
+                Console.WriteLine(aPart);
+            }
+
+            Console.WriteLine("\nParts[3]: {0}", parts[3]);
+
+            Console.WriteLine("\nRemove(\"1534\")");
+
+            // This will remove part 1534 even though the PartName is different, 
+            // because the Equals method only checks PartId for equality.
+            parts.Remove(new Part() {PartId = 1534, PartName = "cogs"});
+
+            Console.WriteLine();
+            foreach (var aPart in threadSafeList)
+            {
+                Console.WriteLine(aPart);
+            }
+            Console.WriteLine("\nRemoveAt(3)");
+            // This will remove the part at index 3.
+            parts.RemoveAt(3);
+
+            Console.WriteLine();
+            foreach (var aPart in threadSafeList)
+            {
+                Console.WriteLine(aPart);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
